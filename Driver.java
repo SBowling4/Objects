2f4bfe430afd3427cb1eecfa127843d6d279c457
SBowling4 <bowlisam001@icloud.com>
2024-11-21 14:05:08 -0600

    added scanner

--- Driver.java
+++ Driver.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
-        
-}
+        Scanner scanner = new Scanner(System.in);
+       
+        scanner.close();
+    }
 }
